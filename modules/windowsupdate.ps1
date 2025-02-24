0bd83934c9e86f7f905d01e51d5aacb2921ffa3d
Tristan BRINGUIER <39345534+MichelBaie@users.noreply.github.com>
2025-02-24 23:33:10 +0100

    Create windowsupdate.ps1

---
+++ modules/windowsupdate.ps1
@@ -0,0 +1,18 @@
+# Vérifier si le script est exécuté avec des privilèges d'administrateur
+if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
+    Write-Error "Ce script doit être exécuté en tant qu'administrateur. Arrêt du script."
+    exit
+}
+
+# Installer le module PSWindowsUpdate s'il n'est pas déjà présent
+if (-not (Get-Module -ListAvailable -Name PSWindowsUpdate)) {
+    Write-Host "Installation du module PSWindowsUpdate..."
+    Install-Module -Name PSWindowsUpdate -Force -Scope AllUsers
+}
+
+# Importer le module PSWindowsUpdate
+Import-Module PSWindowsUpdate
+
+# Lancer la commande pour rechercher, télécharger, installer les mises à jour et redémarrer automatiquement si nécessaire
+Write-Host "Lancement de la mise à jour de Windows..."
+Get-WindowsUpdate -AcceptAll -Install -AutoReboot
