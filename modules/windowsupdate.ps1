ae727de8fd14939cda20b1ccbd6d583642afe83b
Tristan BRINGUIER <39345534+MichelBaie@users.noreply.github.com>
2025-02-24 23:49:29 +0100

    Update windowsupdate.ps1

--- modules/windowsupdate.ps1
+++ modules/windowsupdate.ps1
@@ -1,26 +1,26 @@
-# Vérifier si le script est exécuté avec des privilèges d'administrateur
+# Vérifier si le script est exécuté avec des privilèges d'administrateur.
 if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
     Write-Error "Ce script doit être exécuté en tant qu'administrateur. Arrêt du script."
     exit
 }
 
-# Vérifier si le Package Provider NuGet est installé, sinon l'installer
+# Vérifier si le Package Provider NuGet est installé, sinon l'installer pour installer par la suite PSWindowsUpdate.
 if (-not (Get-PackageProvider -Name NuGet -ErrorAction SilentlyContinue)) {
-    Write-Host "Installation du Package Provider NuGet pour installer par la suite PSWindowsUpdate..."
+    Write-Host "Installation du Package Provider NuGet..."
     Install-PackageProvider -Name NuGet -Force
 } else {
     Write-Host "Le Package Provider NuGet est déjà installé."
 }
 
-# Installer le module PSWindowsUpdate s'il n'est pas déjà présent
+# Installer le module PSWindowsUpdate s'il n'est pas déjà présent.
 if (-not (Get-Module -ListAvailable -Name PSWindowsUpdate)) {
     Write-Host "Installation du module PSWindowsUpdate..."
     Install-Module -Name PSWindowsUpdate -Force -Scope AllUsers
 }
 
-# Importer le module PSWindowsUpdate
+# Importer le module PSWindowsUpdate.
 Import-Module PSWindowsUpdate
 
-# Lancer la commande pour rechercher, télécharger depuis les serveurs de Windows Update, installer les mises à jour et redémarrer automatiquement si nécessaire
+# Lancer la commande pour rechercher, télécharger depuis les serveurs de Windows Update, installer les mises à jour et redémarrer automatiquement si nécessaire.
 Write-Host "Lancement de la mise à jour de Windows..."
-Install-WindowsUpdate -MicrosoftUpdate -AcceptAll -AutoReboot | Out-File "C:\Automatisations\WindowsUpdate.log" -force
+Install-WindowsUpdate -MicrosoftUpdate -AcceptAll -AutoReboot
