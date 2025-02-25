c9b08600dd3485a6462019fca90c5cee8307505f
Tristan BRINGUIER <39345534+MichelBaie@users.noreply.github.com>
2025-02-25 15:42:48 +0100

    Update windowsupdate.ps1
    final working version

--- modules/windowsupdate.ps1
+++ modules/windowsupdate.ps1
@@ -1,3 +1,37 @@
+<#
+.SYNOPSIS
+    Script permettant de mettre à jour Windows et les pilotes automatiquement.
+
+.DESCRIPTION
+    Ce script automatise le processus de mise à jour de Windows et des pilotes.
+
+.AUTHOR
+    Tristan BRINGUIER
+
+.DATE
+    Février 2025
+
+.VERSION
+    1.0
+
+.NOTES
+    Ce script doit être exécuté avec des privilèges administratifs.
+    Si ce n'est pas le cas, il se relancera automatiquement en tant qu'administrateur.
+
+.EXAMPLE
+    .\WindowsUpdate.ps1
+
+.PARAMETER None
+    Ce script ne prend pas de paramètres en entrée.
+
+.OUTPUTS
+    Le script affiche les résultats des mises à jour dans la console.
+
+.INPUTS
+    Le script est succeptible de demander à l'utilisateur des autorisations lors de l'installation des modules.
+#>
+
+
 # Fonction pour vérifier si le script est exécuté en tant qu'administrateur
 function Test-Admin {
     $currentUser = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
@@ -6,17 +40,15 @@ function Test-Admin {
 
 # Si le script n'est pas exécuté en tant qu'administrateur, le relancer avec des privilèges élevés
 if (-not (Test-Admin)) {
-    Write-Host "Relancement du script avec des privilèges administratifs..."
+    Write-Host "Relancement du script avec des privilèges administratifs... (Veuillez accepter l'UAC)"
     Start-Process powershell -ArgumentList "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs
     exit
 }
 
-# Vérifier si le Package Provider NuGet est installé, sinon l'installer pour installer par la suite PSWindowsUpdate.
+# Vérifier si le Package Provider NuGet est installé, sinon l'installer pour installer par la suite le module PSWindowsUpdate.
 if (-not (Get-PackageProvider -Name NuGet -ErrorAction SilentlyContinue)) {
     Write-Host "Installation du Package Provider NuGet..."
     Install-PackageProvider -Name NuGet -Force
-} else {
-    Write-Host "Le Package Provider NuGet est déjà installé."
 }
 
 # Installer le module PSWindowsUpdate s'il n'est pas déjà présent.
@@ -29,5 +61,5 @@ if (-not (Get-Module -ListAvailable -Name PSWindowsUpdate)) {
 Import-Module PSWindowsUpdate
 
 # Lancer la commande pour rechercher, télécharger depuis les serveurs de Windows Update, installer les mises à jour et redémarrer automatiquement si nécessaire.
-Write-Host "Lancement de la mise à jour de Windows..."
+Write-Host "Recherche et installation des mises à jour Windows ainsi que des pilotes de l'ordinateur via les serveurs de Windows Update..."
 Install-WindowsUpdate -MicrosoftUpdate -AcceptAll -AutoReboot
